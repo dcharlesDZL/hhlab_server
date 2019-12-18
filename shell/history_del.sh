a2464299c12456c72369d37aeac31cca3aec6000
dcharlesDZL <liketwins.ok@163.com>
2019-12-18 16:42:22 +0800

    add

--- shell/history_del.sh
+++
@@ -1,9 +0,0 @@
-host='121.40.189.126'
-port=3306
-user='jim_sun'
-password='123456abc'
-database='test'
-
-mysql -h $host -P $port -u $user -p$password -D $database <<EOF
-
-show tables;
--- src/util/mqtt/mqtt_util.lua
+++
@@ -1,9 +0,0 @@
-local _util = {}
-
-_util.callback = function(topic, message)  -- string
-    --print("Topic: " .. topic .. ", message: '" .. message .. "'")
-    --mqtt_client:publish(args.topic_p, message)
-    redis_util.redis_cmd('session',"set","mqtt_message", "t:"..topic.." m:"..message)
-end
-
-return _util
